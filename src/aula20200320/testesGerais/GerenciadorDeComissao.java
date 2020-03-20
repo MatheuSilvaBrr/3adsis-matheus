901402bf781df929a1897ce4f25f233b0f00e60a
Matheus Silva <matheus119599@gmail.com>
2020-03-20 20:01:34 -0300

    implementação exercicio aula 20/03

--- src/aula20200320/testesGerais/GerenciadorDeComissao.java
+++ src/aula20200320/testesGerais/GerenciadorDeComissao.java
@@ -3,7 +3,31 @@ package aula20200320.testesGerais;
 public class GerenciadorDeComissao {
 	
 	public static double calcularComissaoPadrao(double valorDaVenda, double percentualDeComissao) {
-		return valorDaVenda * (percentualDeComissao/100.00);
+		return valorDaVenda  * percentualDeComissao / 100 ;
+	}
+
+	public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda, double percentualDeComissao) {
+		double valorAtingidoDeComissao = valorDaVenda * percentualDeComissao / 100;
+		
+		if(valorAtingidoDeComissao >= valorMinimo) {
+			return valorAtingidoDeComissao;
+		}
+		
+		return  valorMinimo;
+	}
+
+	public static double calcularComissaoComFaixa(double valorMinimo, double valorMaximo, double valorDaVenda, double percentualDeComissao) {
+		double valorAtingidoDeComissao = valorDaVenda * percentualDeComissao / 100;
+		
+		if(valorAtingidoDeComissao <= valorMinimo) {
+			return valorMinimo;
+		}
+		
+		if(valorAtingidoDeComissao >= valorMinimo && valorAtingidoDeComissao <= valorMaximo) {
+			return valorAtingidoDeComissao;
+		}
+		
+		return valorMaximo;
 	}
 
 }
