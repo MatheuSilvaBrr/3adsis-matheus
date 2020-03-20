be9d8d8537909362cf35c835cfe9abd469429cd5
Matheus Silva <matheus119599@gmail.com>
2020-03-20 20:28:08 -0300

    resolução exercicio 20/03

--- src/aula20200320/testesGerais/GerenciadorDeComissao.java
+++ src/aula20200320/testesGerais/GerenciadorDeComissao.java
@@ -2,31 +2,35 @@ package aula20200320.testesGerais;
 
 public class GerenciadorDeComissao {
 	
+	
+	
 	public static double calcularComissaoPadrao(double valorDaVenda, double percentualDeComissao) {
-		return valorDaVenda  * percentualDeComissao / 100 ;
+		return valorDaVenda * percentualDeComissao / 100;
 	}
 
-	public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda, double percentualDeComissao) {
+	public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda,
+			double percentualDeComissao) {
 		double valorAtingidoDeComissao = valorDaVenda * percentualDeComissao / 100;
-		
-		if(valorAtingidoDeComissao >= valorMinimo) {
+
+		if (valorAtingidoDeComissao >= valorMinimo) {
 			return valorAtingidoDeComissao;
 		}
-		
-		return  valorMinimo;
+
+		return valorMinimo;
 	}
 
-	public static double calcularComissaoComFaixa(double valorMinimo, double valorMaximo, double valorDaVenda, double percentualDeComissao) {
+	public static double calcularComissaoComFaixa(double valorMinimo, double valorMaximo, double valorDaVenda,
+			double percentualDeComissao) {
 		double valorAtingidoDeComissao = valorDaVenda * percentualDeComissao / 100;
-		
-		if(valorAtingidoDeComissao <= valorMinimo) {
+
+		if (valorAtingidoDeComissao <= valorMinimo) {
 			return valorMinimo;
 		}
-		
-		if(valorAtingidoDeComissao >= valorMinimo && valorAtingidoDeComissao <= valorMaximo) {
+
+		if (valorAtingidoDeComissao >= valorMinimo && valorAtingidoDeComissao <= valorMaximo) {
 			return valorAtingidoDeComissao;
 		}
-		
+
 		return valorMaximo;
 	}
 
